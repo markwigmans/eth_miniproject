16efa5065706c4917244d22bd30dac51f2385502
Mark Wigmans <mark.wigmans@ximedes.com>
2017-12-10 15:01:12 +0100

    working tests

--- test/voting.js
+++ test/voting.js
@@ -1,31 +1,85 @@
 var Voting = artifacts.require("Voting");
 
 contract('Voting', function (accounts) {
-    it("add voters", function () {
-        var voting;
+    it("add voters", async function() {
         var voter_one = accounts[0];
         var voter_two = accounts[1];
-        return Voting.deployed().then(function (instance) {
-            voting = instance;
-            return voting.giveRightToVote.call(voter_one)
-        }).then(function () {
-            return voting.giveRightToVote.call(voter_two)
-        }).then(function (winners) {
-            assert.lengthOf(winners, 3, "initial votes");
-        })
+
+        const instance = await Voting.new();
+        await instance.giveRightToVote(voter_one);
+        await instance.giveRightToVote(voter_two);
+
+        const winners = await instance.winner();
+        assert.lengthOf(winners, 3, "initial votes");
+        assert.equal(winners[0], 0, "initial vote is 0");
+        assert.equal(winners[1], 1, "initial vote is 1");
+        assert.equal(winners[2], 2, "initial vote is 2");
     });
 
-    it("vote", function () {
-        var voting;
+    it("vote", async function() {
         var voter_one = accounts[0];
-        return Voting.deployed().then(function (instance) {
-            voting = instance;
-            return voting.giveRightToVote(voter_one)
-        }).then(function () {
-            return voting.vote(1, {from: voter_one});
-        }).then(function (winners) {
-            console.log(winners);
-            assert.lengthOf(winners, 1, "winning vote is 1");
-        })
+        const instance = await Voting.deployed();
+        await instance.giveRightToVote(voter_one);
+        await instance.vote(1, {from: voter_one});
+
+        const winners = await instance.winner();
+        assert.lengthOf(winners, 1, "winning vote is 1");
+    });
+
+    it("vote without right", function() {
+        var instance;
+        return Voting.deployed()
+            .then(function (value) {
+                instance = value;
+                return instance.vote(1, {from:  accounts[0]});
+            })
+            .then(function() {
+                    assert(false, 'call with insufficient right should have failed');
+                    return true;
+                },
+                function(e) {
+                    assert.match(e, /VM Exception[a-zA-Z0-9 ]+: invalid opcode/, "call with insufficient right should have raised VM exception");
+                    return true;
+            });
+    });
+
+    it("multiple votes", async function() {
+        const instance = await Voting.new();
+        await instance.giveRightToVote( accounts[0]);
+        await instance.giveRightToVote( accounts[1]);
+        await instance.giveRightToVote( accounts[2]);
+        await instance.giveRightToVote( accounts[3]);
+        await instance.giveRightToVote( accounts[4]);
+        await instance.giveRightToVote( accounts[5]);
+
+
+        await instance.vote(1, {from: accounts[0]});
+        await instance.vote(1, {from: accounts[1]});
+        await instance.vote(2, {from: accounts[2]});
+        await instance.vote(0, {from: accounts[3]});
+        await instance.vote(0, {from: accounts[4]});
+        await instance.vote(1, {from: accounts[5]});
+
+        const winners = await instance.winner();
+        assert.lengthOf(winners, 1, "winning vote is 1");
+        assert.equal(winners[0], 1, "winning vote is 1");
+    });
+
+    it("a draw", async function() {
+        const instance = await Voting.new();
+        await instance.giveRightToVote( accounts[0]);
+        await instance.giveRightToVote( accounts[1]);
+        await instance.giveRightToVote( accounts[2]);
+        await instance.giveRightToVote( accounts[3]);
+
+        await instance.vote(1, {from: accounts[0]});
+        await instance.vote(1, {from: accounts[1]});
+        await instance.vote(2, {from: accounts[2]});
+        await instance.vote(2, {from: accounts[3]});
+
+        const winners = await instance.winner();
+        assert.lengthOf(winners, 2, "winning vote is 1 and 2");
+        assert.equal(winners[0], 1, "winning vote is 1");
+        assert.equal(winners[1], 2, "winning vote is 2");
     });
 });
